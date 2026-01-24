14e5b1d9c7365b8eda9e8da8ea473ed265600914
LEE MING DAO <mingleedao2@gmail.com>
2026-01-25 00:22:02 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package T9Q3;
+
+public class Main {
+    public static void main(String[] args) {
+        RegularPay Jordan = new RegularPay(7.5,8);
+        SpecialPay Vincent = new SpecialPay(7.5, 8);
+
+        System.out.println(Jordan.calculateGivenHours(8));
+        System.out.println(Vincent.calculateGivenHours(8));
+
+        Jordan.display();
+        Vincent.display();
+    }   
+}
