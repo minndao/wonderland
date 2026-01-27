31fb629d0784cf3ce0c8cb186fc6bdd597f56e70
LEE MING DAO <mingleedao2@gmail.com>
2026-01-27 22:26:08 +0800

    Remove pom.xml and add new settings.json and README.md files; implement MyProfile class with personal details.

---
+++ Labs/FOP_Lab01/L1Q2/L1Q2/src/MyProfile.java
@@ -0,0 +1,26 @@
+/**
+ * @author mingl
+ */
+public class MyProfile {
+    public static void main(String[] args) {
+       
+        String fullName = "Lee Ming Dao";
+        String matricNumber = "25006825";
+        String address = "B7 Jalan15a Cinta Sayang Reaort Home 08000 Sungai Petani Kedah Malaysia";
+        String email = "25006825@siswa.um.edu.my";
+        String contactNumber = "+60 11 3053 1120";
+        String course = "Bachelor of Computer Science (Artificial Intelligence)";
+        
+
+        System.out.println("==================================================================================================");
+        System.out.println("                               Executable Personal Profile (L1Q2)                                 ");
+        System.out.println("==================================================================================================");
+        System.out.printf("%-20s: %s\n", "Full Name", fullName);
+        System.out.printf("%-20s: %s\n", "Matric Number", matricNumber);
+        System.out.printf("%-20s: %s\n", "Course/Program", course);
+        System.out.printf("%-20s: %s\n", "Email Address", email);
+        System.out.printf("%-20s: %s\n", "Contact Number", contactNumber);
+        System.out.printf("%-20s: %s\n", "Address", address);
+        System.out.println("==================================================================================================");
+    }}
+     
